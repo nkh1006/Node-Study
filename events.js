63ea281585ba1c3773f79d066cf619bce062e714
NANGGI <mqsora@naver.com>
2021-06-02 10:26:36 +0900

    Add files via upload

---
+++ events.js
@@ -0,0 +1,13 @@
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+// Register a listener
+emitter.on('messageLogged', (arg) => {
+    console.log('Listener called', arg);
+})
+
+// Raise an event
+// emitter.emit('messageLogged', { id: 1, url: 'http://' });
+
+const log = require('./logger');
+log('message');
