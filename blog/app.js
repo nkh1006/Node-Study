5863adb2c65df7d159174893c1ae103faaf48ba3
NANGGI <mqsora@naver.com>
2021-06-09 18:11:02 +0900

    Add files via upload

---
+++ blog/app.js
@@ -0,0 +1,5 @@
+// require('./some.js');
+
+var greetFromSomeJS = require('./some.js');
+
+greetFromSomeJS();
---
+++ blog/some.js
@@ -0,0 +1,9 @@
+// console.log('Greetings from some.js !');
+
+var greet = function() {
+    console.log('Greetings from some.js !');
+};
+
+greet();
+
+module.exports = greet;
