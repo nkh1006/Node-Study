b3bedd45e76687a1a56dd4f143f47563897cf28f
NANGGI <mqsora@naver.com>
2021-06-10 12:28:48 +0900

    Add files via upload

---
+++ async-demo/callback.js
@@ -0,0 +1,30 @@
+console.log('Before');
+getUser(1, getRepositories);
+ console.log('After');
+
+function getRepositories(user) {
+    getRepositories(user.gitHubUserName, getCommits);
+}
+
+function getCommits(repos) {
+    getCommits(repos, displayCommits);
+};
+
+function displayCommits(commits) {
+    console.log(commits);
+}
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log('Reading a user from a database ...');
+        callback({ id: id, gitHubUserName: 'mosh' });
+        // return { id: id, gitHubUserName: 'mosh'};
+    }, 2000);
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log('Calling GitHub API ...');
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+}
