67b945820eae02129009dc274d9c8900b8bce984
NANGGI <mqsora@naver.com>
2021-06-17 18:17:50 +0900

    Add files via upload

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,44 +1,73 @@
-// express class 호출
+const Joi = require('joi');
+const logger = require('./logger');
 const express = require('express');
-
-// 불러온 class로 인스턴스 생성
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(logger);
+
 const courses = [
-    { id: 1, name: 'course1' },
-    { id: 2, name: 'course2' },
-    { id: 3, name: 'course3' },
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' },
 ]
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.send('Hello World !');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+  res.send(courses);
 });
 
 app.post('/api/courses', (req, res) => {
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
 });
 
-app.get('/api/courses', (req, res) => {
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name,
-    };
-    courses.push(course);
-    res.send(course);
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('404 Error !');
+  
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  
+  course.name = req.body.name;
+  res.send(course);
 });
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).require()
+  };
+  return Joi.validate(course, schema);  
+}
+
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('404 Error !');
+  
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+})
 
 app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('not found');
-    res.send(course);
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('404 Error !');
+  res.send(course);
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port} ...`));
+const PORT = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on ${port} PORT !`));
