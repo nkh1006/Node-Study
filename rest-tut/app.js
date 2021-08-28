f58d6cb515b75095d1fa0b423ba25cd2721a2356
NANGGI <mqsora@naver.com>
2021-08-28 22:29:16 +0900

    Add files via upload

--- rest-tut/app.js
+++ rest-tut/app.js
@@ -1,24 +1,30 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+// Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
 
 // Middlewares
-/*
-  app.use('/posts', () => {
-    console.log('This is a middleware running');
-  });
-*/
-
-app.use(auth);
+app.use('/posts', () => {
+  console.log('This is a middleware running');
+});
 
 // ROUTES
 app.get('/', (req, res) => {
   res.send('We are on home');
 });
 
-app.get('/posts', (req, res) => {
-  res.send('We are on posts');
-});
+// Connect To DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true },
+  () => { console.log('connected to DB !');}
+);
 
-// How to we start listening to the server
+// HOW to we start lisitening to the server
 app.listen(3000);
+
