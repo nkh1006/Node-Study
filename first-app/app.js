d2383d8d5618ebf2e273743f3897d0e73e017058
NANGGI <mqsora@naver.com>
2021-09-09 23:55:55 +0900

    Add files via upload

---
+++ first-app/app.js
@@ -0,0 +1,20 @@
+var log = require('./logger');
+
+
+log('message');
+
+/*
+  function sayHello(name) {
+    console.log('Hello ' + name);
+  }
+
+  // sayHello('Mosh');
+
+  console.log(window);
+
+  setTimeout(() => {
+
+  }, 1000);
+
+  var message = '';
+*/
---
+++ first-app/os.js
@@ -0,0 +1,6 @@
+const os = require("os");
+
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+
+console.log('Total Memory: ' + totalMemory);
