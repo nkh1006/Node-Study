a51fc6aba2f168a1cbdf4637ac80a0fb65e212d4
NANGGI <mqsora@naver.com>
2021-09-18 23:21:02 +0900

    Add files via upload

---
+++ poiemaweb/part2.js
@@ -0,0 +1,20 @@
+// ! npm install
+// ? npm i package name
+// ? npm install package name
+
+// ! npm init
+// ? npm init
+// ? npm init -y
+// ? create package.json
+
+// ! package.json
+// ? 여러가지 환경설정 변수를 선언 하고 프로그램에 대한 내용도 적을수 있다.
+
+// ! node emoji module install
+// ? node i node-emoji
+// ? node i node-emoji -g / global setup
+// ? node i --save-dev node-emoji / dev setup
+
+// ! uninstall npm module
+// ? npm uninstall package-name
+// ? npm uninstall -g package-name
