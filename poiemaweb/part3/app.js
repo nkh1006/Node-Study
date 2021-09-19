b72818c78148a91e6bbc82df53302de7c9bcddd9
NANGGI <mqsora@naver.com>
2021-09-19 19:46:28 +0900

    Add files via upload

---
+++ poiemaweb/part3/app.js
@@ -0,0 +1,9 @@
+// ? require (include) module
+// == require('./circle')
+const circle = require('./circle.js');
+
+console.log(`지름이 4인 원의 면적: ${circle.area(4)}`);
+console.log(`지름이 4인 원의 둘레: ${circle.circumference(4)}`);
+
+// ? call app.js
+// ! node app
---
+++ poiemaweb/part3/circle.js
@@ -0,0 +1,6 @@
+// const PI = Math.PI;
+const { PI } = Math;
+
+// ? exports module
+exports.area = (r) => PI * r * r;
+exports.circumference = (r) => 2 * PI * r;
