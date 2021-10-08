c96aa01f5e780f151ea48a14d7507a3bdce140a4
NANGGI <mqsora@naver.com>
2021-10-08 14:08:16 +0900

    Add files via upload

--- node_express_api/index.js
+++ node_express_api/index.js
@@ -1,21 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import usersRoutes from './routes/users.js';
-
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-  console.log(`Server Running on port: http://localhost:${PORT}`);
-});
-
 app.get('/', (req, res) => {
-  console.log('[TEST]!');
-  console.log(users);
-  res.send('Hello from Homepage');
+  console.log('[TEST]');
 });
 
-app.use('/users', usersRoutes);
+app.listen(PORT, () => {
+  console.log(`Server running on port: http://localhost:${PORT}`);
+});
