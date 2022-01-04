b81b321e79ddf0f6beb268b3771c6f2d406bc9e5
NANGGI <mqsora@naver.com>
2022-01-05 00:25:17 +0900

    Add files via upload

---
+++ node-winston2/config/logger.js
@@ -0,0 +1,77 @@
+const winston = require('winston');
+const winstonDaily = require('winston-daily-rotate-file');
+const appRoot = require('app-root-path');
+const process = require('process');
+
+const logDir = `${appRoot}/logs`;
+
+const {
+  combine,
+  timestamp,
+  label,
+  printf
+} = winston.format;
+
+const logFormat = printf(({
+  level,
+  message,
+  label,
+  timestamp
+}) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+/**
+ * Log Level
+ * error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
+ */
+const logger = winston.createLogger({
+  format: combine(
+    label({
+      label: 'System Name'
+    }),
+    timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    logFormat
+  ),
+  transports: [
+    new winstonDaily({
+      level: 'info',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir,
+      filename: `%DATE%.log`,
+      maxFiles: 30,
+      zippedArchive: true
+    }),
+    new winstonDaily({
+      level: 'error',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir,
+      filename: `%DATE%.error.log`,
+      maxFiles: 30,
+      zippedArchive: true
+    }),
+  ],
+  exceptionHandlers: [
+    new winstonDaily({
+      level: 'error',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir,
+      filename: `%DATE%.exception.log`,
+      maxFiles: 30,
+      zippedArchive: true      
+    })
+  ]
+})
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    )
+  }))
+}
+
+module.exports = logger;
