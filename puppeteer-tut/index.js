3bafc9598670efb437427c5da258a82b0ec1167d
NANGGI <mqsora@naver.com>
2022-03-07 18:09:21 +0900

    Add files via upload

--- puppeteer-tut/index.js
+++ puppeteer-tut/index.js
@@ -1,16 +1,46 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://www.naver.com/");
-  // await page.screenshot({ path: "mywebsite.png" });
+  /*
+  await page.goto("https://machadopedro.com/");
+  await page.screenshot({ path: "mywebsite.png" });
 
-  const grapParagraph = await page.evaluate(() => {
-    const pgTag = document.querySelector(".title_area > .title");
-    return pgTag.innerText;
+  const grabParagraph = await page.evaluate(() => {
+    // const pgTag = document.querySelector(".col-lg-6.intro-content p");
+    const tecTag = document.querySelectorAll(".row.back-end ul li");
+    let technologies = [];
+    tecTag.forEach((tag) => {
+      technologies.push(tag.innerText);
+    })
+    return technologies;
   });
+  console.log(grabParagraph);
+  */
 
-  console.log(grapParagraph);
+  /*
+  const grapQuotes = await page.evaluate(() => {
+    const quotes = document.querySelectorAll('.quote');
+    let quotesArr = [];
+    quotes.forEach((quoteTag) => {
+      const quoteInfo = quoteTag.querySelectorAll('span');
+      const actualQuote = quoteInfo[0];
+      const actualAuthor = quoteInfo[1];
+
+      const authorName = actualAuthor.querySelector('small');
+
+      quotesArr.push({ quotes: actualQuote.innerText, author: actualAuthor.innerText });
+    })
+  });
+  */
+
+  await page.goto("https://quotes.toscrape.com/");
+
+  await page.click('a[href="/login"]');
+  await page.type('#username', 'PedroTech', { delay: 100 });
+  await page.type('#password', 'Password123', { delay: 100 });
+
+  await page.click('input[value="Login"]');
   await browser.close();
 })();
