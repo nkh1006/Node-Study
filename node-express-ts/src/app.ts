3f6694920debf5ba81d2766e34f633d864c9ef7a
NANGGI <mqsora@naver.com>
2022-03-30 01:21:03 +0900

    Add files via upload

--- node-express-ts/src/app.ts
+++ node-express-ts/src/app.ts
@@ -1,11 +1,11 @@
-import express, {Application, Request, Response} from 'express';
+import express from 'express';
 
-const app: Application = express();
-const port: number = 3000;
+const app = express();
+const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello world');
-});
+app.get('/', (res, req) => {
+  res.send("Hello world");
+})
 
 app.listen(port, () => {
   console.log(`Connected successfully on port ${port}`);
