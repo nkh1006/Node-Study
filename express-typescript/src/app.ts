649cbc865299f44b39258087cfda865ce3809e0d
NANGGI <mqsora@naver.com>
2022-05-10 03:20:55 +0900

    Add files via upload

--- express-typescript/src/app.ts
+++ express-typescript/src/app.ts
@@ -1,64 +1,75 @@
-import express, {NextFunction, Request, Response} from 'express';
+import { Console } from 'console'
+import express, {NextFunction, Request, Response} from 'express'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
 
-app.route('/api/books')
+/*
+app.route('/')
 .get((req: Request, res: Response) => {
-  return res.send('You make a GET request');
+  return res.send('You make a GET request')
 })
 .post((req: Request, res: Response) => {
-  return res.send('You make a POST request');
+  return res.send('You make a GET request')
 })
 .put((req: Request, res: Response) => {
-  return res.send('You make a PUT request');
+  return res.send('You make a PUT request')
+}),
+((req: Request, res: Response) => {
+  return res.send('You make an PUT request')
 })
-.all((req: Request, res: Response) => {
-  return res.send('You make an X request');
-})
-
-app.get('/health', (req, res) => res.sendStatus(200));
-app.get('/ab*cd', (req, res) => res.send("/ab*cd"));
-app.get('/abc/', (req, res) => res.send("abc"));
-
-const handleGetBookOne = (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.params);
-  next();
-};
-
-const handleGetBookTwo = (req: Request, res: Response, next: NextFunction) => {
-  console.log('second handler');
-  return res.send(req.params);
-};
-
-// app.get('/api/book/:bookId/:authorId', handleGetBookOne, handleGetBookTwo);
-
-app.get(
-  "/api/books/:bookId/:authorId",
-  (req: Request, res: Response, next: NextFunction) => {
-    next();
-  },
-  (req: Request, res: Response, next: NextFunction) => {
-    return res.send(req.params);
-  }
-)
+*/
 
 app.get("/", (req: Request, res: Response) => {
-  return res.redirect("http://example.com");
+  /*
+  return res.json({
+    sucess: true,
+    name: 'TomDoesTech'
+  })
+  */
+ return res.redirect('http://example.com')
+})
+
+function middleware(req: Request, res: Response, next: NextFunction) {
+  // @ts-ignore
+  req.name = 'Tom'
+  /*
+  console.log(req.params);
+
+  next();
+  */
+}
+
+function handleGetBookTwo(req: Request, res: Response, next: NextFunction) {
+  console.log('second handler');
+
+  return res.send(req.params);
+}
+
+app.get('/api/books/:bookId/:authorId', middleware, (req: Request, res: Response, next: NextFunction) => {
+  // @ts-ignore
+  console.log(req.name);
+  next();
 });
+
+/*
+app.get('/health', (req, res) => res.sendStatus(200));
+app.get('/ab*cd', (req, res) => res.send("/ab*cd"));
+app.get(/abc/, (req,res) => res.send("abc"));
 
 app.post('/api/data', (req: Request, res: Response) => {
-  console.log(req.body);
+  console.log(req.body)
 
-  return res.sendStatus(200);
-});
+  return res.sendStatus(200)
+})
 
 app.all('/api/all', (req: Request, res: Response) => {
-  return res.sendStatus(200);
-});
+  return res.sendStatus(200)
+})
+*/
 
 app.listen(3000, () => {
-  console.log(`Application listening at http://localhost:3000`);
-});
+  console.log(`Application listening at http://localhost:3000`)
+})
