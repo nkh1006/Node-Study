71db49206cb46f5bfdca7d70b965723b6bea423d
NANGGI <mqsora@naver.com>
2022-06-20 22:49:11 +0900

    Add files via upload

--- node-pirple/app/index.js
+++ node-pirple/app/index.js
@@ -36,16 +36,57 @@ const server = http.createServer((req, res) => {
   req.on('end', () => {
     buffer += decoder.end();
 
-    // Send the reponse
-    res.end('Hello World');
+    console.log('trimmedPath', trimmedPath);
+    const chosenHandler = typeof(router[trimmedPath] !== 'undefined') ? router[trimmedPath] : handlers.notFound;
+    console.log('router[trimmedPath]', router['sample']);
+
+    const data = {
+      'trimmedPath': trimmedPath,
+      'queryStringObject': queryStringObject,
+      'method': method,
+      'headers': headers,
+      'payload': buffer
+    };
+
+    console.log('chosenHandler', chosenHandler);
+
+    chosenHandler(data, (statusCode, payload) => {
+      statusCode = typeof(statusCode) === 'number' ?  statusCode : 200;
+      payload = typeof(payload) === 'object' ? payload : {};
+      const payloadString = JSON.stringify(payload);
+
+      res.writeHead(statusCode);
+
+      // Send the reponse
+      res.end('Hello World');
+      
+      console.log('Returning this reponse: ', statusCode.payloadString);
+    });
 
     // Log the request path
-    console.log('Request received on path: ' + trimmedPath + ' with method: ' + method);
-    console.log('Querystring: ' + JSON.stringify(queryStringObject) + ' with headers: ' + JSON.stringify(headers));
-    console.log('Request received with this payload: ' + buffer);
+    // console.log('Request received on path: ' + trimmedPath + ' with method: ' + method);
+    // console.log('Querystring: ' + JSON.stringify(queryStringObject) + ' with headers: ' + JSON.stringify(headers));
+    // console.log('Request received with this payload: ' + buffer);
   });
 });
 
 server.listen(3000, () => {
-  console.log("The server is listening on port 300 now");
+  console.log("The server is listening on port 3000 now");
 });
+
+let handlers = {};
+
+// Sample handler
+handlers.sample = (data, callback) => {
+  callback(406, {'name' : 'sample handler'});
+};
+
+// Not found handler
+handlers.notFound = (data, callback) => {
+  callback(404);
+};
+
+// Define a request router
+const router = {
+  'sample': handlers.sample
+}
