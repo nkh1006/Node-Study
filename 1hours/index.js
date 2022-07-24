b1aaeb50376137f69ca9d09fe2bf3b41dc79b61b
NANGGI <mqsora@naver.com>
2022-07-24 23:18:52 +0900

    Add files via upload

---
+++ 1hours/index.js
@@ -0,0 +1,31 @@
+/**
+ * Primary file for the API
+ */
+
+// Dependencies
+const http = require('http');
+const url = require('url');
+
+// The server should respond to all requests with a string
+const server = http.createServer((req, res) => {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Send the reponse
+  res.end('Hello World\n');
+
+  // Log the request path 
+  console.log(`request received on path: ${trimmedPath}`);
+});
+
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, () => {
+  console.log('The server is linstening port 3000 now');
+});
+
+
